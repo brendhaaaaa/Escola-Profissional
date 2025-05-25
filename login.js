84b05247473f257030a9f1f5038bfe5cf61def83
brendha <oros7090228@gmail.com>
2025-05-25 14:53:21 -0300

    login e menu

--- login.js
+++ login.js
@@ -1,23 +1,20 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita o envio do formulário tradicional
+    event.preventDefault(); 
 
     const usernameInput = document.getElementById('username').value.trim();
     const passwordInput = document.getElementById('password').value.trim();
     const errorMessage = document.getElementById('errorMessage');
 
-    // Usuários permitidos com senhas
     const users = {
         'Brendha': '54321',
-        'Ingrid': '12345'
+        'Ingridi': '12345'
     };
 
-    // Validação simples
     if (users[usernameInput] && users[usernameInput] === passwordInput) {
-        // Login válido, redirecionar para a página desejada
-        // Atualize o caminho abaixo para o caminho real da sua página destino
+
         window.location.href = "escola.html";
     } else {
-        // Usuário ou senha inválidos
+
         errorMessage.textContent = "Nome de usuário ou senha inválidos.";
     }
 });
